f85484413b2dca27628105268ee54623ec21e05a
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2023-03-15 15:42:04 -1000

    resctructure resources

---
+++ influxdb.tf
@@ -0,0 +1,63 @@
+###############################################
+##                                           ##
+## INFLUX datasource, folder, and dashboards ##
+##                                           ## 
+###############################################
+
+# provision datasource
+resource "grafana_data_source" "play-ds-influx" {
+  provider = grafana.play
+
+  type          = "influxdb"
+  name          = "play-influx"
+  url           = "https://influxdb.grafana.fun"
+  uid           = "play-influx"
+  json_data_encoded = jsonencode({
+    defaultBucket = "_monitoring", 
+    httpMode      = "POST", 
+    organization  = "grafana", 
+    version       = "Flux"
+  })
+  secure_json_data_encoded = jsonencode({
+    token = "-UGPBXzFUWtwa7M2JD5b8iuA8pVInwH7ms-fBofSa52ddESlvg_oqKohn60x5-sF-OgvWa5BKFpUu8BgY5auGQ=="
+  })
+}
+
+# provision folder
+resource "grafana_folder" "play-folder-influx" {
+  provider = grafana.play
+
+  title = "influxDB"
+}
+
+# provision dashboard
+resource "grafana_dashboard" "play-dashboard-influx-01" {
+  provider = grafana.play
+
+  config_json = file("dashboards/influx/influx-noaa-buoys.json")
+  folder = grafana_folder.play-folder-influx.id
+}
+
+# provision dashboard
+resource "grafana_dashboard" "play-dashboard-influx-02" {
+  provider = grafana.play
+
+  config_json = file("dashboards/influx/influx-usgs-earthquakes.json")
+  folder = grafana_folder.play-folder-influx.id
+}
+
+# provision dashboard
+resource "grafana_dashboard" "play-dashboard-influx-03" {
+  provider = grafana.play
+
+  config_json = file("dashboards/influx/influx-water-samples.json")
+  folder = grafana_folder.play-folder-influx.id
+}
+
+# provision dashboard
+resource "grafana_dashboard" "play-dashboard-influx-04" {
+  provider = grafana.play
+
+  config_json = file("dashboards/influx/influx-internal-metrics.json")
+  folder = grafana_folder.play-folder-influx.id
+}
--- init.tf
+++ init.tf
@@ -1,9 +1,4 @@
-###############################################
-##                                           ##
-## STEP 1: declare initial provider          ##
-##                                           ## 
-###############################################
-
+# declare initial provider        
 terraform {
   required_providers {
     grafana = {
@@ -12,30 +7,6 @@ terraform {
     }
   }
 }
-
-# Declaring the primary provider to be only used for creating the cloud-stack
-provider "grafana" {
-  alias         = "primary"
-
-  cloud_api_key = "eyJrIjoiYjM4MTZiZjM5M2FhMjhjZWNmNDUxMGU1MzM4NGNmYTEyOTQwNjVjNyIsIm4iOiJVWFJFU0VBUkNIIiwiaWQiOjUwMDB9" 
-}
-
-####################################################################
-##                                                                ##
-## STEP 2: create stack, Grafana API key, and set second provider ##
-##                                                                ## 
-####################################################################
-
-# Creating an API key in Grafana instance to be used for creating resources in Grafana instance
-# resource "grafana_cloud_stack_api_key" "play" {
-#   provider = grafana.primary
-
-#   stack_slug = grafana_cloud_stack.play.slug
-#   name       = grafana_cloud_stack.play.name
-#   role       = "Admin" 
-# }
-
-# Declaring the second provider to be used for creating resources in Grafana        
 provider "grafana" {
   alias = "play"
 
