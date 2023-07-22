45727ac9ded1bca7364d2f1eaa1b4365c0797e6e
Matt Abrams <37156449+zuchka@users.noreply.github.com>
2023-07-21 14:37:50 -1000

    refactor dashboard deploy

--- play-frontend/demo-influxdb.tf
+++ play-frontend/demo-influxdb.tf
@@ -53,45 +53,44 @@ resource "grafana_folder" "play-folder-influx" {
   title    = "Demo: InfluxDB 2"
 }
 
-## TODO: can't set folder permissions this way on cloud
-# # set folder permissions
-# resource "grafana_folder_permission" "influx-permission" {
-#   folder_uid = grafana_folder.play-folder-influx.uid
-#   permissions {
-#     role       = "Editor"
-#     permission = "View"
-#   }
+# provision dashbaords
+resource "grafana_dashboard" "influx" {
+  provider = grafana.play
+
+  for_each    = fileset("${path.module}/dashboards/influx", "*.json")
+  config_json = file("${path.module}/dashboards/influx/${each.key}")
+  folder      = grafana_folder.play-folder-influx.id
+}
+
+# # provision dashboard
+# resource "grafana_dashboard" "play-dashboard-influx-01" {
+#   provider = grafana.play
+
+#   config_json = file("dashboards/influx/influx-noaa-buoys.json")
+#   folder      = grafana_folder.play-folder-influx.id
 # }
 
-# provision dashboard
-resource "grafana_dashboard" "play-dashboard-influx-01" {
-  provider = grafana.play
 
-  config_json = file("dashboards/influx/influx-noaa-buoys.json")
-  folder      = grafana_folder.play-folder-influx.id
-}
+# # provision dashboard
+# resource "grafana_dashboard" "play-dashboard-influx-02" {
+#   provider = grafana.play
 
+#   config_json = file("dashboards/influx/influx-usgs-earthquakes.json")
+#   folder      = grafana_folder.play-folder-influx.id
+# }
 
-# provision dashboard
-resource "grafana_dashboard" "play-dashboard-influx-02" {
-  provider = grafana.play
+# # provision dashboard
+# resource "grafana_dashboard" "play-dashboard-influx-03" {
+#   provider = grafana.play
 
-  config_json = file("dashboards/influx/influx-usgs-earthquakes.json")
-  folder      = grafana_folder.play-folder-influx.id
-}
+#   config_json = file("dashboards/influx/influx-water-samples.json")
+#   folder      = grafana_folder.play-folder-influx.id
+# }
 
-# provision dashboard
-resource "grafana_dashboard" "play-dashboard-influx-03" {
-  provider = grafana.play
+# # provision dashboard
+# resource "grafana_dashboard" "play-dashboard-influx-04" {
+#   provider = grafana.play
 
-  config_json = file("dashboards/influx/influx-water-samples.json")
-  folder      = grafana_folder.play-folder-influx.id
-}
-
-# provision dashboard
-resource "grafana_dashboard" "play-dashboard-influx-04" {
-  provider = grafana.play
-
-  config_json = file("dashboards/influx/influx-internal-metrics.json")
-  folder      = grafana_folder.play-folder-influx.id
-}
+#   config_json = file("dashboards/influx/influx-internal-metrics.json")
+#   folder      = grafana_folder.play-folder-influx.id
+# }
